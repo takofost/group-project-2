be6625ebcb99bc3060bd6c925171451031758a2d
Kenny Tran <trank7295@gmail.com>
2019-04-30 21:21:30 -0400

    schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+create database kimado;
+use kimado;
+create table students(
+student_name varchar(20),
+student_rank varchar(20),
+date_joined varchar(8),
+num_classes int(100),
+student_age int (10),
+phone varchar(12),
+email varchar(20),
+student_active boolean default true
+);
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
+
+
+
